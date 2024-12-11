4cc4443cc520c77e705f041726d98568a9746dc8
Erikvilar <erikvilaralves@gmail.com>
2024-12-11 17:18:17 -0300

    change: implementado mapper struct para criação de novos produtos

--- src/main/java/com/ltadcrm/ltadcrm/domain/DTO/domainDTO/ReceivingDTO.java
+++ src/main/java/com/ltadcrm/ltadcrm/domain/DTO/domainDTO/ReceivingDTO.java
@@ -1,5 +1,8 @@
 package com.ltadcrm.ltadcrm.domain.DTO.domainDTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,9 +11,16 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class ReceivingDTO {
+    @JsonProperty("id_recebimento")
     private Long receivingID;
+    @JsonProperty("termo")
     private Long receivingCode;
+    @JsonProperty("lotação")
     private String lotation;
+    @JsonProperty("local")
     private String local;
+    @JsonIgnore
+    @JsonProperty("empSIAFI")
     private String empSiafi;
+    
 }
--- src/main/java/com/ltadcrm/ltadcrm/gateway/CreateAllIEntities.java
+++ src/main/java/com/ltadcrm/ltadcrm/gateway/CreateAllIEntities.java
@@ -1,7 +1,5 @@
 package com.ltadcrm.ltadcrm.gateway;
 
-
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +10,19 @@ import com.ltadcrm.ltadcrm.domain.Items;
 import com.ltadcrm.ltadcrm.domain.Receiving;
 import com.ltadcrm.ltadcrm.domain.Users;
 import com.ltadcrm.ltadcrm.domain.DTO.domainDTO.UpdateDTO;
+import com.ltadcrm.ltadcrm.gateway.mapper.ContactsMapper;
+import com.ltadcrm.ltadcrm.gateway.mapper.CostCenterMapper;
+import com.ltadcrm.ltadcrm.gateway.mapper.DetailsMapper;
+import com.ltadcrm.ltadcrm.gateway.mapper.ItemsMapper;
+import com.ltadcrm.ltadcrm.gateway.mapper.ReceivingMapper;
+import com.ltadcrm.ltadcrm.gateway.mapper.ReceivingMapperImpl;
+import com.ltadcrm.ltadcrm.gateway.mapper.UsersMapper;
 import com.ltadcrm.ltadcrm.repositories.ContactsRepository;
 import com.ltadcrm.ltadcrm.repositories.CostCenterRepository;
 import com.ltadcrm.ltadcrm.repositories.DetailsRepository;
 import com.ltadcrm.ltadcrm.repositories.ItemsRepository;
+import com.ltadcrm.ltadcrm.repositories.ReceivingRepository;
 import com.ltadcrm.ltadcrm.repositories.UsersRepository;
-
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,79 +30,54 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CreateAllIEntities {
 
-    
-
     private final ItemsRepository itemsRepository;
     private final UsersRepository usersRepository;
     private final CostCenterRepository costCenterRepository;
     private final DetailsRepository detailsRepository;
     private final ContactsRepository contactsRepository;
+    private final ReceivingRepository receivingRepository;
+    private final ItemsMapper itemsMapper;
+    private final UsersMapper usersMapper;
+    private final DetailsMapper detailsMapper;
+    private final CostCenterMapper costCenterMapper;
+    private final ContactsMapper contactsMapper;
+    private final ReceivingMapper receivingMapper;
 
     public ResponseEntity<String> create(UpdateDTO updateDTO) {
         try {
- 
+
             Items item = new Items();
-            item.setNumber(updateDTO.getItemsDTO().getNumber());
-            item.setObservation(updateDTO.getItemsDTO().getObservation());
-            item.setPathImage(updateDTO.getItemsDTO().getPathImage());
-            item.setSde(updateDTO.getItemsDTO().getSde());
-            item.setOrder(updateDTO.getItemsDTO().getOrder());
-            item.setProcessSEI(updateDTO.getItemsDTO().getProcessSEI());
-            item.setStatus(updateDTO.getItemsDTO().getStatus());
-            item.setNfInvoice(updateDTO.getItemsDTO().getNfInvoice());
-            item.setValue(updateDTO.getItemsDTO().getValue());
-            item.setLastModification(updateDTO.getItemsDTO().getLastModification());
-            item.setUpdateIn(updateDTO.getUpdateAt());
+            itemsMapper.updateDomainFromDTO(item, updateDTO.getItemsDTO());
+            itemsRepository.save(item); 
 
-     
-            Users user = new Users();
-            user.setUserName(updateDTO.getUsersDTO().getUserName());
-            user.setUserType(updateDTO.getUsersDTO().getUserType());
-            user.setLastModification(updateDTO.getUsersDTO().getLastModification());
+            Users user = new Users(); 
+            usersMapper.updateDomainFromDTO(user, updateDTO.getUsersDTO());
+            item.setUsers(user);
+            usersRepository.save(user); 
 
-            Details description = new Details();
-            description.setBrand(updateDTO.getDetailsDTO().getBrand());
-            description.setDescription(updateDTO.getDetailsDTO().getDescription());
-            description.setLocal(updateDTO.getDetailsDTO().getLocal());
-            description.setModel(updateDTO.getDetailsDTO().getModel());
-            description.setSerial(updateDTO.getDetailsDTO().getSerial());
-            description.setLastModification(updateDTO.getDetailsDTO().getLastModification());
+            Details details = new Details();
+            detailsMapper.updateDomainFromDTO(details, updateDTO.getDetailsDTO());
+            item.setDetails(details);
+            detailsRepository.save(details); 
+            
+            contactsMapper.updateDomainFromDTO(contactsRepository.save(new Contacts()), updateDTO.getContactsDTO());
+            
 
             CostCenter costCenter = new CostCenter();
-            costCenter.setName(updateDTO.getCostCenterDTO().getName());
-            costCenter.setIdentification(updateDTO.getCostCenterDTO().getIdentification());
-            costCenter.setInitialDate(updateDTO.getCostCenterDTO().getInitialDate());
-            costCenter.setEndDate(updateDTO.getCostCenterDTO().getEndDate());
-            costCenter.setLastModification(updateDTO.getCostCenterDTO().getLastModification());
-
-            Contacts contact = new Contacts();
-            contact.setEmail(updateDTO.getContactsDTO().getEmail());
-            contact.setOccupation(updateDTO.getContactsDTO().getOccupation());
-            contact.setPhone(updateDTO.getContactsDTO().getPhone());
-            contact.setLastModification(updateDTO.getContactsDTO().getLastModification());
-            
-            Receiving receiving = new Receiving();
-            receiving.setReceivingCode(updateDTO.getReceivingDTO().getReceivingCode());
-            receiving.setLocal(updateDTO.getReceivingDTO().getLocal());
-            receiving.setEmpSiafi(updateDTO.getReceivingDTO().getEmpSiafi());
-        
-            item.setUsers(user);
-            item.setDetails(description);
+            costCenterMapper.updateDomainFromDTO(costCenter, updateDTO.getCostCenterDTO());
             item.setCostCenter(costCenter);
-            item.setReceiving(receiving);
-
-            usersRepository.save(user);
-            detailsRepository.save(description);
             costCenterRepository.save(costCenter);
-            contactsRepository.save(contact);
-            itemsRepository.save(item);
+
+            Receiving receiving = new Receiving();
+            receivingMapper.updateDomainFromDTO(receiving, updateDTO.getReceivingDTO());
+            item.setReceiving(receiving);
+            receivingRepository.save(receiving);
+
             return ResponseEntity.ok("Item criado");
 
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Ocorreu um erro ao criar o item "+e);
+            return ResponseEntity.badRequest().body("Ocorreu um erro ao criar o item " + e);
         }
     }
-
-   
 
 }
