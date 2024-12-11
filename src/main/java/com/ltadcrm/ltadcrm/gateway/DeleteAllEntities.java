d566a8bd4d60f89cdc01070bfd7c6c1eb005316b
Erikvilar <erikvilaralves@gmail.com>
2024-12-11 17:19:16 -0300

    logic: lógica melhorada ao deletar a row do front end

--- src/main/java/com/ltadcrm/ltadcrm/gateway/DeleteAllEntities.java
+++ src/main/java/com/ltadcrm/ltadcrm/gateway/DeleteAllEntities.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+
 import com.ltadcrm.ltadcrm.domain.Items;
 import com.ltadcrm.ltadcrm.repositories.ItemsRepository;
 
@@ -15,15 +16,16 @@ import lombok.RequiredArgsConstructor;
 public class DeleteAllEntities {
 
     private final ItemsRepository itemsRepository;
-
+    
     public ResponseEntity<String> deleteItem(List<Long> id) {
 
         List<Items> items = itemsRepository.findAllById(id);
         try {
-            if (items.isEmpty()) {
+            if (!items.isEmpty()) {
                 itemsRepository.deleteAll(items);
+                return ResponseEntity.ok("Linha  foi deletada");
             }
-            return ResponseEntity.ok("Linha  foi deletada");
+            return ResponseEntity.noContent().build();
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Ocorreu um erro ao deletar " + e);
         }
