4cbc30941974affef0f6131ae75acf2fb0d62889
Erikvilar <erikvilaralves@gmail.com>
2025-01-21 17:44:25 -0300

    database: nova versao de migration

--- src/main/java/com/ltadcrm/ltadcrm/LtadcrmApplication.java
+++ src/main/java/com/ltadcrm/ltadcrm/LtadcrmApplication.java
@@ -17,13 +17,9 @@ public class LtadcrmApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LtadcrmApplication.class, args);
 
-		System.out.println(calcRandom() + " Valor retornado ");
 
 	}
 
-	private static int calcRandom() {
-		int randomNum = new Random().nextInt(1000);
-		return randomNum;
-	}
+	
 
 }
