571d8733345dbdf10afb7e614a951490da2d1331
Erikvilar <erikvilaralves@gmail.com>
2025-01-31 17:12:51 -0300

    resolving merge

--- src/main/java/com/ltadcrm/ltadcrm/usecases/mapper/CostCenterMapper.java
+++ src/main/java/com/ltadcrm/ltadcrm/usecases/mapper/CostCenterMapper.java
@@ -4,11 +4,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
 import com.ltadcrm.ltadcrm.domain.CostCenter;
-<<<<<<< HEAD:src/main/java/com/ltadcrm/ltadcrm/usecases/mapper/CostCenterMapper.java
-import com.ltadcrm.ltadcrm.domain.DTO.domainDTO.CostCenterDTO;
-=======
->>>>>>> 088196398320d42ee1271c01106582a55410814a:src/main/java/com/ltadcrm/ltadcrm/gateway/mapper/CostCenterMapper.java
-
 import com.ltadcrm.ltadcrm.domain.DTO.domainDTO.CostCenterDTO;
 @Mapper(componentModel = "spring")
 public interface CostCenterMapper {
